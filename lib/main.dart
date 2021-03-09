b163f820bfdd919842176ad0e3eb20611ab1da6a
Jongyun Ahn <jyahn@ptype.co.kr>
2021-03-09 16:32:11 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:clima_flutter/screens/loading_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
